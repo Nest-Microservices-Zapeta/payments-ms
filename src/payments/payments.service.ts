aa97c09a9163d21d1f060b15278ff72e856adf26
David Jonatan <113814145+JONATANZCH@users.noreply.github.com>
2024-05-28 17:12:12 -0600

    fistt commit

---
+++ src/payments/payments.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@nestjs/common';
+import { envs } from 'src/config';
+import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payment-session.dto';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class PaymentsService {
+  private readonly stripe = new Stripe(envs.stripeSecret);
+
+  async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items, orderId } = paymentSessionDto;
+
+    const lineItems = items.map((item) => ({
+      price_data: {
+        currency,
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: Math.round(item.price * 100),
+      },
+      quantity: item.quantity,
+    }));
+
+    const session = await this.stripe.checkout.sessions.create({
+      // Colocar el ID de mi orden
+      payment_intent_data: {
+        metadata: {
+          orderId,
+        },
+      },
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: 'http://localhost:3003/payments/success',
+      cancel_url: 'http://localhost:3003/payments/cancel',
+    });
+
+    return session;
+  }
+
+  async stripeWebhook(req: Request, res: Response) {
+    const sig = req.headers['stripe-signature'];
+    let event: Stripe.Event;
+    // Testing secret key
+    // const endpointSecret =
+    //   'whsec_f3d1fd7863ff9daeaac5602f3777e68cc71843e727900bab4a573b0692a97130';
+    // Production secret key
+    const endpointSecret = 'whsec_OrkJclF0lAN3sVcSHqctqaeXAVP6vG3o';
+
+    try {
+      event = this.stripe.webhooks.constructEvent(
+        req['rawBody'],
+        sig,
+        endpointSecret,
+      );
+    } catch (error) {
+      res.status(400).send(`Webhook Error: ${error.message}`);
+      return;
+    }
+
+    switch (event.type) {
+      case 'charge.succeeded':
+        const chargeSucceeded = event.data.object;
+        console.log({
+          metadata: chargeSucceeded.metadata,
+          orderId: chargeSucceeded.metadata.orderId,
+        });
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+    return res.status(200).json({ sig });
+  }
+}
