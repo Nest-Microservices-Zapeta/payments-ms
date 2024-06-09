ba3cf940a340d66576203ff9efac6e1f10842a4a
David Jonatan <113814145+JONATANZCH@users.noreply.github.com>
2024-06-08 20:39:24 -0600

    chore: Update environment variables for Stripe integration

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -10,6 +10,7 @@ interface EnvVars {
   STRIPE_SUCCESS_URL: string;
   STRIPE_CANCEL_URL: string;
   STRIPE_ENDPOINT_SECRET: string;
+  NATS_SERVERS: string[];
 }
 
 const envVarsSchema = joi
@@ -19,10 +20,14 @@ const envVarsSchema = joi
     STRIPE_SUCCESS_URL: joi.string().required(),
     STRIPE_CANCEL_URL: joi.string().required(),
     STRIPE_ENDPOINT_SECRET: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true)
 
-const { error, value } = envVarsSchema.validate(process.env);
+const { error, value } = envVarsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if(error){
   throw new Error(`Config validation error: \${error.message}`);
@@ -35,4 +40,5 @@ export const envs = {
     stripeSuccessUrl: envVars.STRIPE_SUCCESS_URL,
     stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
     stripeEndpointSecret: envVars.STRIPE_ENDPOINT_SECRET,
+    natsServers: envVars.NATS_SERVERS,
 }
---
+++ src/config/services.ts
@@ -0,0 +1,2 @@
+/* eslint-disable prettier/prettier */
+export const NATS_SERVICE = 'NATS_SERVICE';
